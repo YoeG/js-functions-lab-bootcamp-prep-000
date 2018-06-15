5e2f2a9d5919b82e516cb99e2d7d83beca16171c
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-15 21:18:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ function happyHolidaysTo (name)
 }
 
 
-function
-holidayCountdown (days,holiday)
+function holidayCountdown (days,holiday)
 
 {
-  
+return 'It\'s ${days} days until ${holiday}'
   
 }
